4d24adf9f51cd39ecc90d9a980e63e78582d3a82
ctrujial <cris92ta@gmail.com>
2022-04-03 15:19:35 -0500

    conectado a la base local

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
+from asyncio import Task, tasks
 from sqlite3 import Cursor
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_mysqldb import MySQL
 from config import config
 
@@ -11,13 +12,26 @@ conexion=MySQL(app)
 def listar_cursos():
     try:
         cursor = conexion.connection.cursor()
-        sql = "SELECT codigo, nombre, codigo FROM curso"
+        sql = "SELECT nombre, apellido, cc FROM usuario"
         cursor.execute(sql)
         datos=cursor.fetchall()
-        print(datos)
-        return "ok"
-    except Exception as ex:
+        cursos = []
+        for fila in datos:
+            curso = {'nombre': fila[0],'apellido': fila[1],'cc': fila[2]}
+            cursos.append(curso)
+        return jsonify({'cursos':cursos,'mensaje':"Estos son los cursos Listados!"})
+    except Exception as e:
      return "error"
+
+# @app.route('/task',methods = ['POST'])/// no reconoce methods**
+# def crear_tablas():
+#     titulo = request.json['titulo']
+#     descrpcion = request.json['descripcion']
+
+#     new_Task = Task(titulo, descrpcion)
+#     conexion.session.add(new_Task)
+#     conexion.session.commit()
+    
 
 def PaginaNoEncontrada(error):
     return "<h1>tuki tuki lu lu...</h1>"
